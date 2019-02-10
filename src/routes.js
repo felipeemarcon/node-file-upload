5f0af3f266c299041ac1576338325657914a4c3f
Felipe Marcon <felipeemarcon@gmail.com>
2019-02-10 20:04:14 -0200

    Upload to AWS S3 done

--- src/routes.js
+++ src/routes.js
@@ -6,13 +6,13 @@ const Post = require('./models/Post');
 
 routes.post('/posts', multer(multerConfig).single('file'), async (req, res) => {
 
-  const { originalname: name, size, filename: key, } = req.file;
+  const { originalname: name, size, key, location: url = '' } = req.file;
 
   const post = await Post.create({
     name,
     size,
     key,
-    url: '',
+    url
   });
 
   return res.json(post);
