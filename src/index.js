9eb20220fb246d1860647d93d6c14f27fcb9a830
Felipe Marcon <felipeemarcon@gmail.com>
2019-02-10 23:36:08 -0200

    API finished

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
 // Database setup
 mongoose.connect(
-  "mongodb://localhost:27017/upload", 
+  process.env.MONGO_URL, 
   {
     useNewUrlParser: true
   }
@@ -17,6 +18,10 @@ mongoose.connect(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use(
+  '/files', 
+  express.static(path.resolve(__dirname, '..', 'tmp', 'uploads' ))
+);
 
 app.use(require('./routes'));
 
